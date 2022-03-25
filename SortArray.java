81e40907569c82537d6a82e809536077cb0d0be9
NikhitaBatchu <72651367+NikhitaBatchu@users.noreply.github.com>
2022-03-25 18:05:30 +0530

    Update SortArray.java

--- SortArray.java
+++ SortArray.java
@@ -1,12 +1,19 @@
 public class SortArray {    
     public static void main(String[] args) {        
-            
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the Array Elements :");
+        for (int i = 0; i < n; i++){
+        arr[i] = sc.nextInt(); 
+        }       
         System.out.println("Elements of original array: ");    
-        for (int i = 0; i < arr.length; i++) {     
+        for (int i = 0; i < n; i++) {     
             System.out.print(arr[i] + " ");    
         }       
-        for (int i = 0; i < arr.length; i++) {     
-            for (int j = i+1; j < arr.length; j++) {     
+        for (int i = 0; i < n; i++) {     
+            for (int j = i+1; j < n; j++) {     
                if(arr[i] > arr[j]) {    
                    temp = arr[i];    
                    arr[i] = arr[j];    
@@ -17,7 +24,7 @@ public class SortArray {
           
         System.out.println();       
         System.out.println("Elements of array sorted in ascending order: ");    
-        for (int i = 0; i < arr.length; i++) {     
+        for (int i = 0; i < n; i++) {     
             System.out.print(arr[i] + " ");    
         }    
     }    
