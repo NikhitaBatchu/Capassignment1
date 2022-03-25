4893fa6012a0bd8ff071f6cac70c131b4871a9ea
NikhitaBatchu <72651367+NikhitaBatchu@users.noreply.github.com>
2022-03-25 18:12:39 +0530

    Update FileCopy.java

--- FileCopy.java
+++ FileCopy.java
@@ -1,13 +1,13 @@
  static void fileReadWrite() throws IOException
     {
-        File file = new File("");
-        FileInputStream inputStream = new FileInputStream(file);
+        File file1 = new File("https://github.com/NikhitaBatchu/Capassignment1/blob/main/MinMaxElement.java");
+        FileInputStream inputStream = new FileInputStream(file1);
         Scanner sc = new Scanner(inputStream);
         StringBuffer buffer = new StringBuffer();
         while (sc.hasNext())
             buffer.append("\n"+sc.nextLine());
-        File des = new File("C:\\CapSpace\\src\\com\\company\\capgemini\\Assignment\\CopiedFile.txt");
-        FileWriter writer = new FileWriter(des);
+        File file2 = new File("");
+        FileWriter writer = new FileWriter(file2);
         writer.write(buffer.toString());
         writer.flush();
         System.out.println("File content copied and written successfully.");
