e439a7e17e59470934610af593e0b6c744fc816e
NikhitaBatchu <72651367+NikhitaBatchu@users.noreply.github.com>
2022-03-25 18:11:04 +0530

    Update FileCopy.java

--- FileCopy.java
+++ FileCopy.java
@@ -1,1 +1,14 @@
-
+ static void fileReadWrite() throws IOException
+    {
+        File file = new File("");
+        FileInputStream inputStream = new FileInputStream(file);
+        Scanner sc = new Scanner(inputStream);
+        StringBuffer buffer = new StringBuffer();
+        while (sc.hasNext())
+            buffer.append("\n"+sc.nextLine());
+        File des = new File("C:\\CapSpace\\src\\com\\company\\capgemini\\Assignment\\CopiedFile.txt");
+        FileWriter writer = new FileWriter(des);
+        writer.write(buffer.toString());
+        writer.flush();
+        System.out.println("File content copied and written successfully.");
+    }
